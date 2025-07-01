1bc269a89276f01afb545bfbd72c826a4cbe1ce8
Valdielson Siqueira <valdielson.silva@gmail.com>
2025-07-01 20:47:39 -0300

    feat(product): criação das badges da sessão de lista

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
  <Banner/>
+ <ConteudoPrincipal/>
 </template>
  
 <script>
 import Banner from './components/Banner.vue'
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue'
 
 export default {
   name: 'App',
   components: {
-    Banner
+    Banner,
+    ConteudoPrincipal
   }
 }
 </script>
